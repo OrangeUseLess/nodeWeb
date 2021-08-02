273dfae9751d8a329741d067e59f0020f20a2359
yaoxiao <2206133238@qq.com>
2021-08-02 14:45:47 +0800

    将所有页面从公共文件夹中抽离出来，独立在views文件夹中。

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const port = 3000;
 const path = require('path');
 
 app.get('/', (req,res) => {
-    res.redirect('http://localhost:3000/static/src/index/index.html');
+    res.redirect(`http://localhost:${port}/views/index/index.html`);
 })
 
 // 提供静态文件, /static 是一个虚拟文件路径
-// app.use('/static', express.static('public'));
 // 一下代码在访问静态文件时，使用的是绝对路径。
 app.use('/static', express.static(path.join(__dirname, 'public')));
+
+// 页面
+app.use('/views',express.static('views'));
 
 app.listen(port, () => {
     console.log(`已连接到 http://localhost:${port}`);

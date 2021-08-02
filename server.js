c51628fbc9ca9b3035841d6103494be993c11fef
yaoxiao <2206133238@qq.com>
2021-08-02 11:41:51 +0800

    修改访问localhost:3000时，自动跳转到首页

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const port = 3000;
 const path = require('path');
 
 app.get('/', (req,res) => {
-    res.send('HELLO WORD!');
+    res.redirect('http://localhost:3000/static/src/index/index.html');
 })
-
 
 // 提供静态文件, /static 是一个虚拟文件路径
 // app.use('/static', express.static('public'));
 // 一下代码在访问静态文件时，使用的是绝对路径。
 app.use('/static', express.static(path.join(__dirname, 'public')));
+
 
 app.listen(port, () => {
     console.log(`已连接到 http://localhost:${port}`);

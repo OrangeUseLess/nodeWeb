9edd19128c116611ecd6cb8b85f13da2ab003695
yaoxiao <2206133238@qq.com>
2021-08-02 11:55:39 +0800

    代码整理了一下

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.get('/', (req,res) => {
 // 一下代码在访问静态文件时，使用的是绝对路径。
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-
 app.listen(port, () => {
     console.log(`已连接到 http://localhost:${port}`);
 })
